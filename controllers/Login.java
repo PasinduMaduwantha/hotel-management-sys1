29ea18b2a798938b629083ce132216964983b5da
Pasindu Madhuwantha <73535785+PasinduMaduwantha@users.noreply.github.com>
2022-02-22 11:50:22 +0530

    Add admin form,home and forgot password method

--- controllers/Login.java
+++ controllers/Login.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import connections.Select;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,6 +10,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import javax.swing.*;
+import java.sql.ResultSet;
 
 public class Login {
     public TextField txtEmail;
@@ -41,7 +43,51 @@ public class Login {
         }
         else if (email.equals("admin") && password.equals("1234")){
             check  = 1;
+            try{
+                Stage loginStage = new Stage();
+                Parent root = FXMLLoader.load(this.getClass().getResource("/forms/AdminForm.fxml"));
+                loginStage.setScene(new Scene(root,1300,500));
+                loginStage.setTitle("Admin");
+                Stage stage = (Stage) txtEmail.getScene().getWindow();
+                stage.close();
+                loginStage.show();
 
+            }
+            catch (Exception e){
+                JOptionPane.showMessageDialog(null, e);
+            }
+        }
+        else{
+            ResultSet resultSet = Select.resultSet("SELECT * FROM users WHERE email = '"+email+"' AND password = '"+password+"'");
+            try{
+                if (resultSet.next()){
+                    check = 1;
+                    if (resultSet.getString(7).equals("true")){
+                        try{
+                            Stage loginStage = new Stage();
+                            Parent root = FXMLLoader.load(this.getClass().getResource("/forms/Home.fxml"));
+                            loginStage.setScene(new Scene(root,1300,500));
+                            loginStage.setTitle("Home");
+                            Stage stage = (Stage) txtEmail.getScene().getWindow();
+                            stage.close();
+                            loginStage.show();
+
+                        }
+                        catch (Exception e){
+                            JOptionPane.showMessageDialog(null, e);
+                        }
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(null, "Wait for admin approval");
+                    }
+                }
+            }
+            catch (Exception e){
+                JOptionPane.showMessageDialog(null, e);
+            }
+        }
+        if (check == 0){
+            JOptionPane.showMessageDialog(null, "Invalid email or password");
         }
     }
 
