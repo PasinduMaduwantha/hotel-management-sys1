68bfb4f2c1dfa05d04ae9fdbeb61b25b1ace46c0
Pasindu Madhuwantha <73535785+PasinduMaduwantha@users.noreply.github.com>
2022-03-08 01:07:07 +0530

    Add Customer check in

--- main.java
+++ main.java
@@ -16,8 +16,8 @@ public class main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         try{
-            Parent root = FXMLLoader.load(this.getClass().getResource("forms/Admin.fxml"));
-            primaryStage.setScene(new Scene(root,1300,500));
+            Parent root = FXMLLoader.load(this.getClass().getResource("forms/Home.fxml"));
+            primaryStage.setScene(new Scene(root,600,400));
             primaryStage.setTitle("Signup");
             primaryStage.show();
 
@@ -30,20 +30,28 @@ public class main extends Application {
     public static void main(String[] args) {
 
         Connection connection = null;
-        Statement statement = null;
+        Statement statement1 = null;
+        Statement statement2 = null;
+        Statement statement3 = null;
         try {
             connection = ConnectionProvider.getConnection();
-            statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users (name varchar (200),email varchar (200),password varchar (50),sequrityQuestion varchar (500),answer varchar (200),address varchar (200),status varchar (20))" );
+            statement1 = connection.createStatement();
+            statement2 = connection.createStatement();
+            statement3 = connection.createStatement();
+            statement1.executeUpdate("CREATE TABLE IF NOT EXISTS users (name varchar (200),email varchar (200),password varchar (50),sequrityQuestion varchar (500),answer varchar (200),address varchar (200),status varchar (20))" );
+            statement2.executeUpdate("CREATE TABLE IF NOT EXISTS rooms(roomNo varchar (10),roomType varchar(200),bed varchar (200),price int,status varchar (20))");
+            statement3.executeUpdate("CREATE TABLE IF NOT EXISTS customers(bookingId int, name varchar (200),mobilNo varchar (20),nationality varchar (200),gender varchar (50),email varchar (200),idProof varchar (200), address varchar (500),checkIn varchar (50),roomNo varchar (10),bed varchar (200),roomType varchar (200),pricePerDay int(10),numberOdDayStay int (10),totalAmount varchar (200),chekOut varchar (50))");
 //            JOptionPane.showMessageDialog(null, "Table Created Successfully");
         }
         catch (Exception e){
-            JOptionPane.showMessageDialog(null, "Can not Create table named users");
+            JOptionPane.showMessageDialog(null, "Can not Create table(s)");
         }
         finally {
             try {
                 connection.close();
-                statement.close();
+                statement1.close();
+                statement2.close();
+                statement3.close();
             }
             catch (Exception e){
                 JOptionPane.showMessageDialog(null, "Can not close the connection");
